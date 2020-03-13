b7870169c1282900521f7fcfe3c29ea7881afc39
Nara <nara@nara-3519.lan>
2020-03-12 22:27:14 -0600

    remove unused code

--- server/app.js
+++ server/app.js
@@ -3,13 +3,6 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-// app.use(express.static(__dirname + '/public'));
-
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/public/index.html')
-// });
-
-
 http.listen(3000, () => {
     console.log('Listening on port *: 3000');
 });
