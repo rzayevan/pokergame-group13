ca00fc2459ff568208d836bc9b48818d017c379b
Wilfredo Jr. Pasadilla <wilfredojp3@gmail.com>
2020-03-15 10:17:33 -0600

    6

--- poker-pals/src/App.vue
+++ poker-pals/src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
+<<<<<<< HEAD
     <Header/>
     
     <Table/>
     <Table/>
     <Table/>
+=======
+    <router-view></router-view>
+>>>>>>> master
   </div>
 </template>
 
 <script>
+<<<<<<< HEAD
 import Table from './components/Table.vue'
 import Header from './components/Header.vue'
 
@@ -19,6 +24,23 @@ export default {
     Header
   },
 
+=======
+///// Uncomment the following lines if you want to use them as components in this file /////
+// import Login from './components/Login.vue';
+// import Poker from './components/Poker.vue';
+// import Profile from './components/Profile.vue';
+// import Reports from './components/Reports.vue';
+// import Tables from './components/Tables.vue';
+export default {
+  name: 'App',
+  components: {
+    // Login,
+    // Poker,
+    // Profile,
+    // Reports,
+    // Tables
+  }
+>>>>>>> master
 }
 
 </script>
--- poker-pals/src/main.js
+++ poker-pals/src/main.js
@@ -6,13 +6,29 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import App from './App.vue'
+<<<<<<< HEAD
 // Install BootstrapVue
 Vue.use(BootstrapVue)
     // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+=======
+import VueRouter from 'vue-router'
+import Routes from './routes'
+>>>>>>> master
 
+Vue.use(VueRouter);
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes: Routes
+});
+
 new Vue({
+<<<<<<< HEAD
     render: h => h(App),
 }).$mount('#app')
+=======
+  render: h => h(App),
+  router: router
+}).$mount('#app')
+>>>>>>> master
