53ee374fe403315821ab38db7bd49307091d1f23
Wilfredo Jr. Pasadilla <wilfredojp3@gmail.com>
2020-03-15 10:42:17 -0600

    Update main.js

--- poker-pals/src/main.js
+++ poker-pals/src/main.js
@@ -5,30 +5,23 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import App from './App.vue'
-<<<<<<< HEAD
 // Install BootstrapVue
 Vue.use(BootstrapVue)
     // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
-=======
+
+import App from './App.vue'
 import VueRouter from 'vue-router'
 import Routes from './routes'
->>>>>>> master
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false
 
 const router = new VueRouter({
-  routes: Routes
+    routes: Routes
 });
 
 new Vue({
-<<<<<<< HEAD
     render: h => h(App),
+    router: router
 }).$mount('#app')
-=======
-  render: h => h(App),
-  router: router
-}).$mount('#app')
->>>>>>> master
