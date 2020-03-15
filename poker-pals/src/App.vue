559270ec4fc4f42d3c7720dc6f21c41ded1aabf4
Wilfredo Jr. Pasadilla <wilfredojp3@gmail.com>
2020-03-15 10:36:57 -0600

    Resolved Conflicts

--- poker-pals/src/App.vue
+++ poker-pals/src/App.vue
@@ -1,36 +1,18 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <Header/>
-    
-    <Table/>
-    <Table/>
-    <Table/>
-=======
     <router-view></router-view>
->>>>>>> master
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import Table from './components/Table.vue'
-import Header from './components/Header.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Table,
-    Header
-  },
-
-=======
 ///// Uncomment the following lines if you want to use them as components in this file /////
 // import Login from './components/Login.vue';
 // import Poker from './components/Poker.vue';
 // import Profile from './components/Profile.vue';
 // import Reports from './components/Reports.vue';
 // import Tables from './components/Tables.vue';
+//import Table from './components/Table.vue';
+
 export default {
   name: 'App',
   components: {
@@ -39,8 +21,8 @@ export default {
     // Profile,
     // Reports,
     // Tables
+    //Table
   }
->>>>>>> master
 }
 
 </script>
