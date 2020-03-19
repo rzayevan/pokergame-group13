2c8b319241d4481670ed7bfd79388a0e5c494275
Nara <nara@nara-3716.lan>
2020-03-18 18:11:39 -0600

    css changes

--- poker-pals/src/components/UserNavbar.vue
+++ poker-pals/src/components/UserNavbar.vue
@@ -3,12 +3,14 @@
         <div id="app-logo">
             <p id="app-logo-text">Poker Pals</p>
         </div> 
-        <div class="chip-logo">
-            <img src="../assets/logo.png"/>
-        </div>
-        <div class="chip-amount">1,200,450</div>
-        <div class="userIcon">
-            <img src="../assets/logo.png"/>
+        <div id="user-info">
+            <div id="chip-info">
+                <img id="chip-img" src="../assets/chip.png"/>
+                <p id="chip-amount">1,200,450</p>
+            </div>
+            <div id="user-logo">
+                <img id="user-img" src="../assets/player_icon_1.png"/>
+            </div>
         </div>
     </div>
 </template>
@@ -16,6 +18,8 @@
 <style scoped>
     @import url('https://fonts.googleapis.com/css?family=Pacifico&display=swap');
     #navigation-bar {
+        display: flex;
+        flex-direction: row;
         width: 100%;
         height: 8%;
         background-color: #01B0D9;
@@ -32,6 +36,38 @@
         float: left;
         padding-top: 5px;
         padding-left: 30px;
+    }
+    #user-info {
+        display: flex;
+        flex-direction: row;
+        padding-top: 4px;
+        padding-right: 20px;
+    }
+
+    #chip-info {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding-right: 50px;
+    }
+
+    #chip-amount {
+        font-family: 'Roboto';
+        color: white;
+        font-size: 22px;
+        font-style: italic;
+    }
+
+    #chip-img {
+        width: 30px;
+        height: 30px;
+    }
+
+    #user-img {
+        margin-top: 5px;
+        margin-bottom: 5px;
+        width: 45px;
+        height: 45px;
     }
 </style>
 
