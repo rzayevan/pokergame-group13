b2de04685c22992f2d2cd938b842883ca2559962
Kimberly Chan <chankk@ucalgary.ca>
2020-03-21 20:45:55 -0600

    Added admin nav bar.

---
+++ poker-pals/src/components/AdminNavbar.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav>
+        <b-navbar toggleable="sm">
+            <b-navbar-brand to="reports" class="mx-3">Poker Pals <span class="p-1">admin</span> </b-navbar-brand>
+            <b-navbar-nav class="ml-auto mx-3" >
+                <b-nav-item to="/" class="b-nav-text">
+                    <span class="material-icons"> logout </span>
+                </b-nav-item>
+            </b-navbar-nav>
+        </b-navbar>
+    </nav>
+</template>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Pacifico&display=swap');
+    @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+    nav {
+        color: white;
+        background-color: #01B0D9;
+        padding: 0;
+    }
+
+    .navbar-brand {
+        font-family: 'Pacifico', sans-serif;
+        color: white;
+        font-size: 28px;
+    }
+
+
+    .navbar-brand span {
+        background-color: #bf214b;
+        font-family: 'Roboto', sans-serif;
+        font-size: 16px;
+    }
+
+    .material-icons {
+        color: white;
+    }
+
+</style>
+
+<script>
+    export default {
+        name: "AdminNavbar.vue",
+    };
+</script>
