b2def3e63f14a2d47c5bf0c787473c9553660d1e
Kimberly Chan <chankk@ucalgary.ca>
2020-03-25 09:53:02 -0600

    Added a public (not signed in) nav bar.

---
+++ poker-pals/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav>
+        <b-navbar>
+            <b-navbar-brand href="#" class="mx-3">Poker Pals</b-navbar-brand>
+        </b-navbar>
+    </nav>
+</template>
+
+<style scoped>
+    nav {
+        color: white;
+        background-color: #01B0D9;
+        padding: 0;
+    }
+
+    .navbar-brand {
+        font-family: 'Pacifico', sans-serif;
+        color: white;
+        font-size: 28px;
+    }
+</style>
+<script>
+    export default {
+        name: "Navbar.vue",
+    };
+</script>
