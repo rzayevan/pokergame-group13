1dbddb8ab21b8e8e316b2d6b81e51a9118f6a148
Nara <nara@nara-3716.lan>
2020-03-31 16:01:44 -0600

    add sign out button to user nav bar

--- poker-pals/src/components/Tables.vue
+++ poker-pals/src/components/Tables.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="table">
-    <div class="table-description">
-        <div class = "table-stakes">
-            <p> 20,000 </p> 
-            <p> 1000/2000 </p>
-        </div> 
-        <div class = "chips-pic">
-          <img alt="Vue logo" src="../assets/chip.png">
-        </div> 
-    </div> 
-    <div class="table-details">
-        <div class = "table-name">
-            <p> Table 1</p>
-        </div>
-        <div class = "seats">
-            <p> 6/6 </p>
+    <div id="tables-page">
+        <UserNavbar/>
+        <div class="table">
+            <div class="table-description">
+                <div class = "table-stakes">
+                    <p> 20,000 </p> 
+                    <p> 1000/2000 </p>
+                </div> 
+                <div class = "chips-pic">
+                <img alt="Vue logo" src="../assets/chip.png">
+                </div> 
+            </div> 
+            <div class="table-details">
+                <div class = "table-name">
+                    <p> Table 1</p>
+                </div>
+                <div class = "seats">
+                    <p> 6/6 </p>
+                </div>
+            </div>
         </div>
     </div>
-  </div>
 </template>
 
 <script>
+import UserNavbar from "./navbars/UserNavbar"
 export default {
   name: 'Tables',
-
+  components: {
+      UserNavbar
+  }
 }
 </script>
 
