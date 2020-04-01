798bcad0b71a17a0a443e7f254d06989ddd047df
Kimberly Chan <chankk@ucalgary.ca>
2020-03-31 20:28:55 -0600

    Modified Chat UI to use Bootstrap (responsive).

--- poker-pals/src/components/pokerComponents/Chat.vue
+++ poker-pals/src/components/pokerComponents/Chat.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="chatBox" v-bind:class="{ chatBoxFull: full, chatBoxHalf: !full }"> <!--the chat container holding all the chat elements together-->
-            <div class="title" v-bind:class="{ titleFull: full, titleHalf: !full }">
-                <div class="title_text centerText">{{ tableName }}</div>
-            </div>
-            <div class="messagesBoxBorder" v-bind:class="{ messagesBoxBorderFull: full, messagesBoxBorderHalf: !full }"> <!--contains all the chat messages in the form of a list-->
-                <div class="messagesBox" v-bind:class="{ messagesBoxFull: full, messagesBoxHalf: !full }">
-                    <!--these buttons are not permanent, only for testing functionality-->
+    <b-container class="container">
+        <b-row class="title" align-v="center" no-gutters>
+            <h3 class="col-12">{{ tableName }}</h3>
+        </b-row>
+        <b-row class="chatBox">
+            <b-col>
+                <!--these buttons are not permanent, only for testing functionality-->
+                <b-row v-show="false">
                     <button class="testButton" v-on:click="$parent.logIn(1000)">log in as userID:1000</button>
                     <button class="testButton" v-on:click="$parent.logIn(2000)">log in as userID:2000</button>
                     <button class="testButton" v-on:click="$parent.logIn(3000)">log in as userID:3000</button>
@@ -13,161 +14,98 @@
                     <button class="testButton" v-on:click="$parent.logIn(5000)">log in as userID:5000</button>
                     <button class="testButton" v-on:click="$parent.logIn(6000)">log in as userID:6000</button>
                     <button class="testButton" v-on:click="$parent.joinTable()">join table 0</button>
-                    
-
-                    <div v-for="item in items" :key="item.message"> <!--display each message according to whom is the sender-->
-                        <ChatMessage v-bind:you="item.id === userID ? true : false" v-bind:name="item.name" v-bind:message="item.message" v-bind:messageId="item.id"/>
-                        <div class="chatSpace">space</div>
-                    </div>
-                </div>
-            </div>
-            <div class="inputBoxBorder" v-bind:class="{ inputBoxBorderFull: full, inputBoxBorderHalf: !full }"> <!--the input of the chat, allows for sending messages-->
-                <div class="inputBox">
-                    <input type="text" class="input" placeholder="Type a message">
-                    <button class="submitButton centerText">SEND</button>
-                </div>
-            </div>
-        </div>
+                </b-row>
+                <b-row id="messages" >
+                    <ChatMessage v-for="item in items"
+                                 :key="item.id"
+                                 v-bind:you="item.senderID === userID"
+                                 v-bind:name="item.name"
+                                 v-bind:message="item.message"
+                                 v-bind:messageId="item.id"
+                    />
+                </b-row>
+            </b-col>
+        </b-row>
+        <b-row class="inputBox py-1">
+            <input type="text" class="input col-8 " placeholder="Type a message">
+            <button type="button" class="btn col-4">SEND</button>
+        </b-row>
+    </b-container>
 </template>
 
 <style scoped>
-    .chatSpace{
-        float: left;
-        width: 50%;
-        color: transparent;
-        font-size: 0.5vw;
+    .container {
+        height: 100%;
+        background-color: #F2F3F5;
     }
-    .chatBox{
-        float: left;
-        width: 30%;
-        background: cyan;
-    }
-    .chatBoxHalf{
-        height: 46.5%;
-    }
-    .chatBoxFull{
-        height: 93%;
-    }
-    .title{
-        float: left;
+
+    .title {
         width: 100%;
-        background: #eeeeee;
-        background: black;
-        margin: 0%;
+        height: 10%;
+        background: #F2F3F5;
     }
-    .titleHalf{
-        height: 12%;
-    }
-    .titleFull{
-        height: 6%;
-    }
-    .title_text{
-        width: 99%;
-        height: 90%;
-        margin: 0.5% 0% 0% 0.5%;
-        background: #eeeeee;
-        font-size: 1.75vw;
-    }
-    .messagesBoxBorder{
-        width: 100%;
-        background: black;
-    }
-    .messagesBoxBorderHalf{
-        height: 85%;
-    }
-    .messagesBoxBorderFull{
-        height: 92.5%;
-    }
-    .messagesBox{
-        float: left;
-        width: 99%;
-        background: #eeeeee;
-        margin: 0% 0% 0% 0.5%;
+
+    .chatBox {
+        background-color: white;
+        height: 80%;
         overflow-y: auto;
     }
-    .messagesBoxHalf{
-        height: 85%;
+
+    #messages {
+        overflow-y: auto;
     }
-    .messagesBoxFull{
-        height: 93.2%;
-    }
-    .inputBoxBorder{
-        width: 100%;
-        background: black;
-    }
-    .inputBoxBorderHalf{
-        height: 15%;
-    }
-    .inputBoxBorderFull{
-        height: 7.5%;
-    }
-    .inputBox{
-        float: left;
-        width: 98.9%;
-        height: 92%;
+
+    .inputBox {
         background: white;
-        margin: 0.2% 0% 0% 0.5%;
+        height: 10%;
     }
-    .input{
-        float: left;
-        width: 78%;
-        height: 93%;
-        margin: 0%;
-        overflow: hidden;
+
+    .input {
         border: none;
-        background: transparent;
-        font-size: 1.5vw;
     }
-    .submitButton{
-        float: left;
-        width: 20%;
-        height: 70%;
-        background: #01b0d9;
-        border: 1px solid black;
-        border-radius: 15%/30%;
+
+    .btn {
+        background-color: #01B0D9;
         color: white;
-        margin: 2% 0% 0% 0%;
-        font-size: 1vw;
     }
-    .centerText{
-        display : flex;
-        align-items : center;
-        justify-content: center;
-    }
+
 </style>
 
 <script>
-import ChatMessage from './ChatMessage.vue';
+    import ChatMessage from './ChatMessage.vue';
 
-export default {
-    name: "Chat",
-    components: {
-        ChatMessage,
-    },
-    props: [
-        'full', 'tableName', 'userID'
-    ],
-    data() {
-        return {
-            items: [ // right now chat does not receive messages, either it will use props or have its own socket functions
-                // sample messages stored in chat
-                { id: 1000, name: 'Mark123', message: 'Foo kjh g n hy kb jk gbn g n g n ihg n g h b jh gi h gv tgb g' },
-                { id: 2000, name: 'John456', message: 'Barh g n hy kb jk gbn g n g n ihg n g h b jh' },
-                { id: 3000, name: 'Luke854', message: 'no' },
-                { id: 3000, name: 'Luke854', message: 'hellofgd ggf df gd gh rf tr r gh g fd gh t t t   gg er g  gt g rt g gtg g trd g g g dr gt gdr g f t hg fdt ghtrf gh f h yh' },
-                { id: 3000, name: 'Luke854', message: 'hello  t t   gg er g  gt g rt g gtg g trd g g g dr gt gdr g f t hg fdt ghtf f g fd g fg h ' },
-                { id: 3000, name: 'Luke854', message: 'hello rf gh fd hg g t g g g  f hhg f hdfh' },
-                { id: 3000, name: 'Luke854', message: 'hello r g r gt g t   f' },
-                { id: 3000, name: 'Luke854', message: 'hello' },
-            ],
+    export default {
+        name: "Chat",
+        components: {
+            ChatMessage,
+        },
+        data() {
+            return {
+                tableName: 'Big Fish',
+                userID: 1000,
+                items: [ // right now chat does not receive messages, either it will use props or have its own socket functions
+                    // sample messages stored in chat
+                    { id: 1, senderID: 1000, name: 'Mark123', message: 'Hi!' },
+                    { id: 2, senderID: 2000, name: 'John456', message: 'gl hf!' },
+                    { id: 3, senderID: 3000, name: 'Luke854', message: 'no' },
+                    { id: 4, senderID: 3000, name: 'Luke854', message: '>:(' },
+                    { id: 5, senderID: 3000, name: 'John456', message: 'oh okay... ' },
+                    { id: 6, senderID: 3000, name: 'Mark123', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+                    { id: 7, senderID: 3000, name: 'Luke854', message: 'f' },
+                    { id: 8, senderID: 3000, name: 'Luke854', message: 'asdsad' },
+                    { id: 9, senderID: 1000, name: 'Mark123', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+                    { id: 10, senderID: 3000, name: 'Luke854', message: 'f' },
+                    { id: 11, senderID: 3000, name: 'Mark123', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+                    { id: 12, senderID: 3000, name: 'Luke854', message: 'f' },
+                ],
+            }
+        },
+        methods:{
+            openReport(name, messageId){ // when a message is clicked on and the report symbol is clicked, a call back is made to open up the report box
+                // called by a ChatMessage child
+                // pass the message along to the parent
+                this.$parent.openReport(name, messageId);
+            }
         }
-    },
-    methods:{
-        openReport(name, messageId){ // when a message is clicked on and the report symbol is clicked, a call back is made to open up the report box
-            // called by a ChatMessage child
-            // pass the message along to the parent
-            this.$parent.openReport(name, messageId);
-        }
-    }
-};
+    };
 </script>
