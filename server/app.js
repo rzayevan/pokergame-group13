e8d5cfae01c9dc11fe302cb2c16a7ddf39da04bc
devongillis <devon.gillis@ucalgary.ca>
2020-04-04 15:41:07 -0600

    finished up merge conflicts

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 let User = require("./model/User.js");
-const { v4: uuidv4 } = require('uuid');
 let PokerController = require("./controllers/PokerController.js");
 let ReportController = require("./controllers/ReportController.js");
 let pokerController, reportController;
@@ -22,12 +21,13 @@ http.listen(3000, () => {
     pokerController = new PokerController(numberOfEachRoom);
 
     reportController = new ReportController();
+
+    console.log('Listening on port *: 3000');
 });
 
 io.on('connection', (socket) => {
+    console.log("Client connected.");          
     socket.emit("connected", "Hello from server");
-    socket.emit("serve-tables", pokerController.rooms);
-    console.log(pokerController.rooms);
 
     socket.on('add-new-user', function(user) {
         // create a new user if the email provided is unique
@@ -51,7 +51,7 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         console.log('disconnected');
         pokerController.disconnectFromTable(io, socket);
     });
@@ -71,16 +71,16 @@ io.on('connection', (socket) => {
     
     // start of added code to talk with poker.vue
     // these next two functions are temporary, need to login and join table via login page and tables page
-    socket.on('joinRoomRequest', function(msg) { // a user wishes to join a room/table
+    socket.on('joinRoomRequest', function(msg){ // a user wishes to join a room/table
         pokerController.joinRoom(io, socket, msg);
     });
 
     // each player will send a message upon a game play button click, this will check whether or not it is the player's turn and if the move is valid
-    socket.on('turnDecision', function(msg) {
+    socket.on('turnDecision', function(msg){
         pokerController.turnDecision(io, socket, msg);
     });
 
-    socket.on('exitRoomRequest', function(msg) {
+    socket.on('exitRoomRequest', function(msg){
         pokerController.exitRoomRequest(io, socket, msg);
     });
 });
