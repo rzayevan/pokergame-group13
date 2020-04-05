4a07312a9a6956b1a9833271f4228c7373d92c79
devongillis <devon.gillis@ucalgary.ca>
2020-04-05 09:29:32 -0600

    merge fixes

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 let User = require("./model/User.js");
+const { v4: uuidv4 } = require('uuid');
 let PokerController = require("./controllers/PokerController.js");
 let ReportController = require("./controllers/ReportController.js");
 let pokerController, reportController;
@@ -21,19 +22,17 @@ http.listen(3000, () => {
     pokerController = new PokerController(numberOfEachRoom);
 
     reportController = new ReportController();
-
-    console.log('Listening on port *: 3000');
 });
 
 io.on('connection', (socket) => {
-    console.log("Client connected.");          
     socket.emit("connected", "Hello from server");
+    socket.emit("serve-tables", pokerController.rooms);
 
     socket.on('add-new-user', function(user) {
         // create a new user if the email provided is unique
         if (!UserUtils.emailExists(user)) {
             let newUser = new User();
-            newUser.CreateNewUser(user.username, user.password, user.email, UserUtils.createUserIcon());
+            newUser.CreateNewUser(user.username, user.password, user.email);
             DataAccessLayer.AddUserToFile(newUser);
             socket.emit("alert text", "Successfully signed up!");
         } else {
@@ -43,17 +42,15 @@ io.on('connection', (socket) => {
 
     socket.on('authenticate user', function(user) {
         // authenticate the user if the credentials provided exist in the stored data
-        let result = UserUtils.credentialsMatch(user);
-        if (result.matchFound) {
-            socket.emit("authenticated", result.userID);
+        if (UserUtils.credentialsMatch(user)) {
+            socket.emit("authenticated", user);
         } else {
             socket.emit("alert text", "Authentication failed. Please try again.");
         }
     });
 
-    socket.on('disconnect', function () {
+    socket.on('disconnect', function() {
         console.log('disconnected');
-        pokerController.disconnectFromTable(io, socket);
     });
 
     socket.on('request reports', function() {
@@ -65,22 +62,18 @@ io.on('connection', (socket) => {
         reportController.retrieveReports(this, true);
     });
 
-    socket.on('serveRoomList', function() {
-        socket.emit("receiveRoomList", pokerController.getRoomList());
-    });
-    
     // start of added code to talk with poker.vue
     // these next two functions are temporary, need to login and join table via login page and tables page
-    socket.on('joinRoomRequest', function(msg){ // a user wishes to join a room/table
+    socket.on('joinRoomRequest', function(msg) { // a user wishes to join a room/table
         pokerController.joinRoom(io, socket, msg);
     });
 
     // each player will send a message upon a game play button click, this will check whether or not it is the player's turn and if the move is valid
-    socket.on('turnDecision', function(msg){
+    socket.on('turnDecision', function(msg) {
         pokerController.turnDecision(io, socket, msg);
     });
 
-    socket.on('exitRoomRequest', function(msg){
+    socket.on('exitRoomRequest', function(msg) {
         pokerController.exitRoomRequest(io, socket, msg);
     });
 });
